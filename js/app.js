ad3baf02bdb50d1c2398967b677c904b515e60b5
caritobd91 <barbozacaro@gmail.com>
2018-02-18 18:19:46 -0500

    Added additional animations

--- js/app.js
+++ js/app.js
@@ -2,5 +2,9 @@ var tl1 = new TimelineMax();
 
 tl1.from('#logo', 1.1, { top: -100, ease: Back.easeOut.config(1.2), y: -100 });
 
+tl1.from('#bag', 1.2, { delay:0.5, ease: Back.easeOut.config(1.7), y: 500}, {opacity:0})
+.to('#bag', 1.5, { delay: 0.5, ease: Back.easeOut.config(0.2), y: 500});
 
-TweenLite.from('#bag', 1.5, { delay:0.5, ease: Expo.easeOut, y: 500}, {opacity:0});
+tl1.from('#premium', 1, {delay: -1, ease: Back.easeOut.config(1.7), x: -300})
+.from('#chocolates', 1, {delay: -1, ease: Back.easeOut.config(1.7), x: 300});
+
