38fdbff15ffb0906087f388cfd188f9b56fa64f5
caritobd91 <barbozacaro@gmail.com>
2018-02-18 18:29:51 -0500

    Finished animation for premium and chocolates

--- js/app.js
+++ js/app.js
@@ -2,9 +2,12 @@ var tl1 = new TimelineMax();
 
 tl1.from('#logo', 1.1, { top: -100, ease: Back.easeOut.config(1.2), y: -100 });
 
-tl1.from('#bag', 1.2, { delay:0.5, ease: Back.easeOut.config(1.7), y: 500}, {opacity:0})
+tl1.from('#bag', 1.2, { delay:0.5, ease: Back.easeOut.config(1.7), y: 400}, {opacity:0})
 .to('#bag', 1.5, { delay: 0.5, ease: Back.easeOut.config(0.2), y: 500});
 
 tl1.from('#premium', 1, {delay: -1, ease: Back.easeOut.config(1.7), x: -300})
-.from('#chocolates', 1, {delay: -1, ease: Back.easeOut.config(1.7), x: 300});
+.from('#chocolates', 1, {delay: -1, ease: Back.easeOut.config(1.7), x: 300})
+.to('#premium', 1, { delay: 0.5, ease: Back.easeOut.config(0.2), x: -300})
+.to('#chocolates', 1, { delay: -1, ease: Back.easeOut.config(0.2), x: 300});
+
 
